7e7f03d03cbbcc23c7955d1e5d3e69d4f1e8b6ff
Mantas <tekorius.mantas@gmail.com>
2019-07-31 23:43:10 +0300

    tweaked new incident form

--- src/NewIncident.js
+++ src/NewIncident.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import {Form, FormGroup, Label, Input } from 'reactstrap';
+import {Form, FormGroup, Label, Input, Button, Container} from 'reactstrap';
 import AppNavbar from './AppNavbar';
 
 export default class NewIncident extends React.Component {
   render() {
     return <div><AppNavbar/>
-      <Form>
-        <FormGroup>
-          <Label for="name">Email</Label>
+    <Container fluid>
+      <Form inline>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Label for="name" className="mr-sm-2">Email</Label>
           <Input type="text" name="name" id="name" placeholder="Incident name" />
         </FormGroup>
-        <FormGroup>
-           <Label for="exampleEmail">Email</Label>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+           <Label for="date" className="mr-sm-2">Date</Label>
            <Input type="date" name="date" id="date" placeholder="Incident date" />
          </FormGroup>
+         <Button>Submit</Button>
       </Form>
+      </Container>
       </div>
       }
   }
